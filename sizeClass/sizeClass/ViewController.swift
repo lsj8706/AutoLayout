30759dee5e2b3adc558b2fba8b11a71b7eacf7f2
Lee SeJin <lsj8706@naver.com>
2022-01-02 15:41:31 +0900

    Size class

---
+++ sizeClass/sizeClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sizeClass
+//
+//  Created by User on 2022/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
